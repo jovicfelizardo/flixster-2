fa74fc3498a90fc8dc2dbfe1bbbe8d2fa0edef84
Meryl Marasigan <meryl0729@gmail.com>
2020-01-21 22:02:55 -0800

    initial commit (not fully operational)

---
+++ flixster/MoviesCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoviesCell.swift
+//  flixster
+//
+//  Created by Meryl Marasigan on 1/21/20.
+//  Copyright © 2020 Meryl Marasigan. All rights reserved.
+//
+
+import UIKit
+
+class MoviesCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
